423f6438f0e8bcfdbb3ab239a9b0e886f4498fec
Ray Shih <mnf.shih@gmail.com>
2016-07-13 22:52:50 +0800

    add backtracking

--- n12stamp/stamp.c
+++ n12stamp/stamp.c
@@ -3,13 +3,48 @@
 
 int denom[INT_MAX];
 int dp[INT_MAX];
+int usage[INT_MAX];
+int k;
+int counterOfSolution;
+
+void backtracking(int curr, int left, int denomIndex)
+{
+  int i, j;
+
+  if (curr < 0 || left < 0 || denomIndex >= k) {
+    // out of range
+    return;
+  }
+
+  if (curr == 0) {
+    // found answer
+
+    counterOfSolution++;
+    printf("(%d)\n", counterOfSolution);
+    for (i = 0; i < k; i++) {
+      if (usage[i] > 0) {
+        printf("%d x %d\n", denom[i], usage[i]);
+      }
+    }
+
+    return;
+  }
+
+  // use
+  usage[denomIndex]++;
+  backtracking(curr - denom[denomIndex], left - 1, denomIndex);
+  usage[denomIndex]--;
+
+  // not use
+  backtracking(curr, left, denomIndex + 1);
+}
 
 int main(int argc, const char *argv[])
 {
-  int k;
   int n;
   int i, j;
   int from;
+  int total;
 
   while (scanf("%d", &k) != EOF) {
 
@@ -38,8 +73,18 @@ int main(int argc, const char *argv[])
       }
     }
 
+    total = dp[n];
+
+    // init for backtracking
+    for (i = 0; i < k; i++) {
+      usage[i] = 0;
+    }
+    counterOfSolution = 0;
+
+    backtracking(n, total, 0);
+
     // output
-    printf("%d\n", dp[n]);
+    printf("total:%d\n\n", total);
   }
 
   return 0;
